9826d9a975d4548e8d0360061af08f611ec90c1a
Riccardo Kiss <36801806+RiccardoKiss@users.noreply.github.com>
2022-07-21 12:58:25 +0200

    Added MUI to List form and form validation

--- src/pages/AddList.tsx
+++ src/pages/AddList.tsx
@@ -1,15 +1,20 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import { styled } from '@mui/material/styles';
 import { Typography } from '@mui/material';
+import Paper from '@mui/material/Paper';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import FormControl from '@mui/material/FormControl';
 import FormHelperText from '@mui/material/FormHelperText';
 import Input from '@mui/material/Input';
 import InputLabel from '@mui/material/InputLabel';
+import { Formik, Field, Form, FormikHelpers, useFormik } from 'formik';
+import * as yup from 'yup';
 
 import { mockapiUrl } from '../utils/mockapiUrl';
 
@@ -23,6 +28,24 @@ const MyToolBar = styled(Toolbar)(() => ({
   justifyContent: 'space-between',
 }));
 
+const Item = styled(Paper)(() => ({
+  backgroundColor: '#f2bb72',
+  padding: '3%',
+  //justifyContent: 'space-evenly',
+}));
+
+const validationSchema = yup.object({
+  listTitle: yup
+    .string()
+    .required('List title is required'),
+  itemTitle: yup
+    .string()
+    .required('Task title is required'),
+  itemTask: yup
+    .string()
+    .required('Task description is required'),
+});
+
 const AddList = () => {
   const fullDate = new Date();
   const day = fullDate.getDate();
@@ -30,15 +53,25 @@ const AddList = () => {
   const year = fullDate.getFullYear();
   const time = fullDate.getHours() + ':' + fullDate.getMinutes();
 
-  const [title, setTitle] = useState("");
+  const [formData, setFormData] = useState({});
 
-  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTitle(event.target.value);
-  };
+  const formik = useFormik({
+    initialValues: {
+      listTitle: '',
+      itemTitle: '',
+      itemTask: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+      setFormData(values);
+    },
+  });
 
-  const handleSubmit = () => {
-    
-  };
+  useEffect(() => {
+    if(Object.keys(formData).length != 0)  // if object is not empty
+      console.log(formData);
+  }, [formData]);
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -51,15 +84,58 @@ const AddList = () => {
       </MyAppBar>
       <Grid container direction="column" spacing={2} justifyContent="center" alignItems="center">
         <Grid item>
-          <Box component="form" sx={{'& > :not(style)': { m: 1 }}} noValidate autoComplete="off">
-            <FormControl variant="standard">
-              <InputLabel htmlFor="component-simple">Title</InputLabel>
-              <Input id="component-simple" value={title} onChange={handleTitleChange} />
-            </FormControl>
-          </Box>
-        </Grid>
-        <Grid item>
-          <Button href='/' color='primary' variant='contained' onClick={handleSubmit}>Submit</Button>
+          <Item elevation={6} >
+              <form onSubmit={formik.handleSubmit}>
+                <Grid container  px={2} py={1} spacing={3} direction="column" justifyContent="space-evenly" alignItems="center">
+                  <Grid item>
+                    <TextField 
+                      id="listTitle" 
+                      name="listTitle" 
+                      label="List Title" 
+                      variant="filled"
+                      size="small"
+                      color={formik.values.itemTask ? "success" : "primary"}
+                      value={formik.values.listTitle}
+                      onChange={formik.handleChange}
+                      error={formik.touched.listTitle && Boolean(formik.errors.listTitle)}
+                      helperText={formik.touched.listTitle && formik.errors.listTitle}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <TextField 
+                      id="itemTitle" 
+                      name="itemTitle" 
+                      label="Item Title" 
+                      variant="filled"
+                      size="small"
+                      color={formik.values.itemTask ? "success" : "primary"}
+                      value={formik.values.itemTitle}
+                      onChange={formik.handleChange} 
+                      error={formik.touched.itemTitle && Boolean(formik.errors.itemTitle)}
+                      helperText={formik.touched.itemTitle && formik.errors.itemTitle}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <TextField 
+                      id="itemTask" 
+                      name="itemTask" 
+                      label="Item Task" 
+                      variant="standard"
+                      multiline
+                      rows={3}
+                      color={formik.values.itemTask ? "success" : "primary"}
+                      value={formik.values.itemTask}
+                      onChange={formik.handleChange}
+                      error={formik.touched.itemTask && Boolean(formik.errors.itemTask)}
+                      helperText={formik.touched.itemTask && formik.errors.itemTask}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <Button color='primary' variant='contained' type='submit'>SUBMIT</Button>
+                  </Grid>
+                </Grid>
+              </form>
+          </Item>
         </Grid>
       </Grid>
     </Box>
